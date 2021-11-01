d829699449ef424f8325de7714cc1968d0342158
sandecodes <muhammadsandy74@gmail.com>
2021-11-01 07:37:25 +0700

    Simplify Blade Templating File & Folder Part 2

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('includes.meta')
+    <title>@yield('title') | Landing Page</title>
+    
+    {{-- Favicon --}}
+    <link rel="apple-touch-icon" href="">
+    <link rel="shortcut icon" type="image/x-icon" href="">
+
+    @stack('before-style')
+    {{-- style --}}
+    @include('includes.style')
+    
+    @stack('after-style')
+</head>
+<body>
+    @include('includes.header')
+        @yield('content')
+    @include('includes.footer')
+
+    @stack('before-script')
+    {{-- script --}}
+    @include('includes.script')
+
+    @stack('after-script')
+</body>
+</html>
